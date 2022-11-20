10e3f3dc0b1737b68ad9bec70613a1ce005c7d12
Igor <dont@bzzr.me>
2022-11-20 17:18:26 +0000

    Add pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: "test-pm2",
+      script: "bin/www",
+    },
+  ],
+  deploy: {
+    production: {
+      key: "keys/test-pm2-key",
+      user: "deploy",
+      host: ["test-pm2"],
+      ref: "origin/main",
+      repo: "https://github.com/i9or/test-pm2.git",
+      path: "/home/deploy/app",
+      "post-deploy": "npm i",
+    },
+  },
+};
